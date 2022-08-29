f271728105b5a787f2691e5410e366aa397538b6
Corrie Stroup <corrie.stroup@gmail.com>
2022-08-29 20:58:28 +0700

    It's working! Clicking the icons plays the game the result changes the score dynamically and the announcements tell the user the results of the match indefinitely. The divs change shapes as numbers get bigger though, which isn't ideal. Would like to make WIN green, LOSE red and DRAW yellow.

--- scripts.js
+++ scripts.js
@@ -1,26 +1,20 @@
-const choices = document.querySelectorAll('button');
-
-choices.forEach((button) => {
-
-    button.addEventListener('click', () => {
-        alert(button.id);
-    });
-});
-
 const rock = document.getElementById('rock-pic');
 const paper = document.getElementById('paper-pic');
 const scissors = document.getElementById('scissors-pic');
 
 rock.addEventListener('click', () => {
     rpsGame("rock"); // it should take input and choose rock
+    updateScore(winNumber,loseNumber,drawNumber,eachRound);
 });
 
 paper.addEventListener('click', () => {
     rpsGame("paper");
+    updateScore(winNumber,loseNumber,drawNumber,eachRound);
 });
 
 scissors.addEventListener('click', () => {
     rpsGame("scissors");
+    updateScore(winNumber,loseNumber,drawNumber,eachRound);
 });
 
 
@@ -42,15 +36,15 @@ function rpsGame(userThrow){
                 // if the userThrow is scissors --> log "Lose" + increment score[2]
                 if(userThrow === "rock"){
                     drawNumber++;
-                    //return "Rock-Rock! It's a tie!";
+                    eachRound =  "The computer chose ROCK and you also chose ROCK! It's a TIE!";
                 } else if (userThrow === "paper"){
                     winNumber++;
-                    //return "Rock-Paper! You win! Way to go!";
+                    eachRound = "The computer chose ROCK and you chose PAPER! You WIN! Way to go!";
                 } else if (userThrow === "scissors"){
                     loseNumber++;
-                    //return "Rock-Scissors! You lose! Get got.";
+                    eachRound = "The computer chose ROCK and you chose SCISSORS! You LOSE! Get got.";
                 } else {
-                    return "Rock Error: Something unexpected rockurred.";
+                    eachRound = "Rock Error: Something unexpected rockurred.";
                 }
                 break;
             // in the event that computerThrow is 1:
@@ -60,15 +54,15 @@ function rpsGame(userThrow){
                 // if the userThrow is scissors --> log "Win" + increment score[0]
                 if(userThrow === "paper"){
                     drawNumber++;
-                    //return "Paper-Paper! It's a tie!";
+                    eachRound = "The computer chose PAPER and you also chose PAPER! It's a TIE!";
                 } else if (userThrow === "scissors"){
                     winNumber++;
-                    //return "Paper-Scissors! You win! Way to go!";
+                    eachRound = "The computer chose PAPER and you chose SCISSORS! You WIN! Way to go!";
                 } else if (userThrow === "rock"){
                     loseNumber++;
-                    //return "Paper-Rock! You lose! Get got.";
+                    eachRound = "The computer chose ROCK and you chose PAPER! You LOSE! Get got.";
                 } else {
-                    return "Paper Error: Something tear-able has happened.";
+                    eachRound = "Paper Error: Something tear-able has happened.";
                 }
                 break;
             // in the event that computerThrow is 2:
@@ -78,39 +72,45 @@ function rpsGame(userThrow){
                 // if the userThrow is scissors --> log "Tie" + increment score[1]
                 if(userThrow === "scissors"){
                     drawNumber++
-                    //return "Scissors-Scissors! It's a tie!";
+                    eachRound = "The computer chose SCISSORS and you also chose SCISSORS! It's a TIE!";
                 } else if (userThrow === "rock"){
                     winNumber++
-                    //return "Scissors-Rock! You win! Way to go!";
+                    eachRound = 'The computer chose SCISSORS and you chose ROCK! You WIN! Way to go!';
                 } else if (userThrow === "paper"){
                     loseNumber++
-                    //return "Scissors-Paper! You lose! Get got.";
+                    eachRound = "The computer chose SCISSORS and you chose PAPER! You LOSE! Get got.";
                 } else {
-                    //return "Scissor Error: Scissor me timbers! Something untoward occurred!";
+                    eachRound = "Scissor Error: Scissor me timbers! Something untoward occurred!";
                 }
                 break;
             // set default message to "something horribly wrong has happened"
             default:
-                return "Something horribly wrong has happened. (default)";
+                eachRound = "Something horribly wrong has happened. (default)";
                 break;
         }
     // end function
     }
 
     let result = rockPaperScissors(userThrow);
-    alert(`${winNumber} + ${loseNumber} + ${drawNumber}`);
+    //alert(`${winNumber} + ${loseNumber} + ${drawNumber}`);
     
     }
 
+function updateScore(wins, losses, draws, announcement){
+        document.querySelector('#win-id').textContent = wins;
+        document.querySelector('#lose-id').textContent = losses;
+        document.querySelector('#draw-id').textContent = draws;
+        document.querySelector('#scroll').textContent = announcement;
+    }
+
+const roundAnnouncement = document.getElementById('scroll');
+let eachRound = "Beat the computer...";    
 
 const winScore = document.getElementById('win-id');
 let winNumber = 0;
-winScore.textContent = winNumber;
 
 const loseScore = document.getElementById('lose-id');
 let loseNumber = 0;
-loseScore.textContent = loseNumber;
 
 const drawScore = document.getElementById('draw-id');
 let drawNumber = 0;
-drawScore.textContent = drawNumber;
